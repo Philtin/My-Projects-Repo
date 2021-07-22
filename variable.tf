947dbe5e5e199aa8fa7fd7bf4e71814b119b12f3
Philtin <86083465+Philtin@users.noreply.github.com>
2021-07-22 13:46:09 -0500

    Escape

---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable OAuthToken {
+    type = string
+    default = ""
+    description = "my token"
+}
+
+variable source_repo_github_owner {
+     type = string 
+     description = "my github name"
+
+}
+
+variable source_repo_github{
+     type = string 
+     description = "my repo name" 
+
+    }
